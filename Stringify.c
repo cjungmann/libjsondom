ba5f819769231c0f72b4acba1380ff8083e3837d
Chuck Jungmann <chuck@cpjj.net>
2026-01-15 17:22:33 -0600

    Most everything used generic stringify after converting float and integer to save as strings.

--- Stringify.c
+++ Stringify.c
@@ -11,6 +11,8 @@ int JNode_stringify_generic(const JNode *node,
                             JDataType type,
                             const char *value)
 {
+   int retval = 0;
+
    if (node == NULL)
       jn_error = JNE_NULL_NODE;
    else if (node->type != type)
@@ -19,7 +21,6 @@ int JNode_stringify_generic(const JNode *node,
    {
       // No calling errors
       jn_error = JNE_SUCCESS;
-      int retval = 0;
 
       if (value)
       {
@@ -38,11 +39,9 @@ int JNode_stringify_generic(const JNode *node,
             buffer[copylen] = '\0';
          }
       }
-
-      return retval;
    }
 
-   return 0;
+   return retval;
 }
 
 int JNode_stringify_null(const JNode *node, char *buffer, int bufflen)
@@ -62,35 +61,7 @@ int JNode_stringify_false(const JNode *node, char *buffer, int bufflen)
 
 int JNode_stringify_string(const JNode *node, char *buffer, int bufflen)
 {
-   int retval = 0;
-
-   if (node == NULL)
-      jn_error = JNE_NULL_NODE;
-   else if (node->type != DT_STRING)
-      jn_error = JNE_INVALID_TYPE;
-   else
-   {
-      // No calling errors
-      jn_error = JNE_SUCCESS;
-
-      const char *value = (const char*)node->payload;
-      retval = strlen(value);
-
-      if (buffer && bufflen > 0)
-      {
-         int copylen;
-         if (bufflen < retval)
-            copylen = bufflen - 1;
-         else
-            copylen = retval;
-
-         if (copylen > 1)
-            memcpy(buffer, value, copylen);
-         buffer[copylen] = '\0';
-      }
-   }
-
-   return retval;
+   return JNode_stringify_generic(node, buffer, bufflen, DT_STRING, (char*)node->payload);
 }
 
 void limited_long_copy(long lval, char **ptr, char *end)
@@ -111,61 +82,12 @@ void limited_long_copy(long lval, char **ptr, char *end)
 
 int JNode_stringify_integer(const JNode *node, char *buffer, int bufflen)
 {
-   int retval = 0;
-
-   if (node == NULL)
-      jn_error = JNE_NULL_NODE;
-   else if (node->type != DT_INTEGER)
-      jn_error = JNE_INVALID_TYPE;
-   else
-   {
-      // No calling errors
-      jn_error = JNE_SUCCESS;
-
-      // Copy value, track and massage negative numbers
-      long lval = *(long*)node->payload;
-      bool negative = lval < 0;
-      lval = labs(lval);
-
-      // Determine char count required for full copy:
-      // Needs to be at least the terminating '\0'
-      retval = 1;
-      if (negative)
-         ++retval;
-
-      while (lval > 0)
-      {
-         ++retval;
-         lval /= 10;
-      }
-
-      // Reuse lval to save the absolute value of negative number
-      lval = *(long*)node->payload;
-      if (negative)
-         lval = labs(lval);
-
-      if (buffer && bufflen > 0)
-      {
-         char *ptr = buffer;
-         char *end = buffer + bufflen - 1;
-
-         if (negative)
-         {
-            *ptr = '-';
-            ++ptr;
-         }
-
-         limited_long_copy(lval, &ptr, end);
-         *end = '\0';
-      }
-   }
-
-   return retval;
+   return JNode_stringify_generic(node, buffer, bufflen, DT_INTEGER, (char*)node->payload);
 }
 
 int JNode_stringify_float(const JNode *node, char *buffer, int bufflen)
 {
-   return 0;
+   return JNode_stringify_generic(node, buffer, bufflen, DT_FLOAT, (char*)node->payload);
 }
 
 int JNode_stringify_property(const JNode *node, char *buffer, int bufflen)
