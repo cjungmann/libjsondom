ace2063605b98972861eff6ce0c04ca602bc46ce
Chuck Jungmann <chuck@cpjj.net>
2026-02-10 12:54:12 -0600

    Remove helper functions that are no longer needed after exposing real jd_Node struct.

--- jsondom.h
+++ jsondom.h
@@ -49,47 +49,8 @@ typedef struct jd_ParseError_s {
    const char *message;      ///< description of error
 } jd_ParseError;
 
-
-/**
- * @brief Indexes of relations to a given jd_Node for use with
- *       jd_get_relation
- *
- * The values go up matching the direction of the relation in a
- * clockwise direction:
- * 
- * 0 up    (12:00) parent node
- * 1 right (03:00) next sibling
- * 2 down  (06:00) first child
- * 3 left  (09:00) previous sibling
- * also special index
- * 4 for pointer to last child (for appending new children)
- */
-typedef enum jd_Relation_e {
-   JD_PARENT = 0,
-   JD_NEXT,
-   JD_FIRST,
-   JD_PREVIOUS,
-   JD_LAST
-} jd_Relation;
-
-
 bool jd_parse_file(int fh, jd_Node **new_tree, jd_ParseError *pe);
 void jd_destroy(jd_Node **node);
-
-jd_Node* jd_get_relation(jd_Node *node, jd_Relation relation);
-
-jd_Node* parent(jd_Node *node);
-jd_Node* nextSibling(jd_Node *node);
-jd_Node* firstChild(jd_Node *node);
-jd_Node* prevSibling(jd_Node *node);
-jd_Node* lastChild(jd_Node *node);
-
-jd_Type jd_id_type(const jd_Node *node);
-const char *jd_id_name(const jd_Node *node);
-const void* jd_generic_value(const jd_Node *node);
-
-int jd_node_value_length(const jd_Node *node);
-int jd_node_value(const jd_Node *node, char *buffer, int bufflen);
 
 void jd_serialize(int jd_out, const jd_Node *node, int indent);
 
